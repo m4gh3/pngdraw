02a2b713d881356d0cd895e0fa06262664382614
maghe <lorenzo.magherini.00@gmail.com>
2020-07-02 00:20:13 +0200

    modificato:             src/png_plot.c

--- src/png_plot.c
+++ src/png_plot.c
@@ -4,7 +4,7 @@ void plot(png_buffer_ut *png_buf, float x0, float x1, float y0, float y1, int w0
 {
 	for(int i=0; i < w1-w0+1; i++ )
 	{
-		float x = (x1-x0)*(((float)i)/(w0-w1)) + x0;
+		float x = (x1-x0)*(((float)i)/(w1-w0)) + x0;
 		float y = (h1-h0)*(f(x, data )-y0)/(y1-y0) + h0;
 		png_buf->row_pointers[(int)y][(i+w0)*3]   |= color&0xff;
 		png_buf->row_pointers[(int)y][(i+w0)*3+1] |= (color>>8)&0xff;
