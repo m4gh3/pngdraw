45c4b0d4df4bab1b6af12af62c4bbe10225c9bd0
m4gh3 <lorenzo.magherini.00@gmail.com>
2020-07-02 00:49:39 +0200

    modified:   src/png_plot.c

--- src/png_plot.c
+++ src/png_plot.c
@@ -6,8 +6,11 @@ void plot(png_buffer_ut *png_buf, float x0, float x1, float y0, float y1, int w0
 	{
 		float x = (x1-x0)*(((float)i)/(w1-w0)) + x0;
 		float y = (h1-h0)*(f(x, data )-y0)/(y1-y0) + h0;
-		png_buf->row_pointers[(int)y][(i+w0)*3]   |= color&0xff;
-		png_buf->row_pointers[(int)y][(i+w0)*3+1] |= (color>>8)&0xff;
-		png_buf->row_pointers[(int)y][(i+w0)*3+2] |= (color>>16)&0xff;
+		if( (h0<=y) && (y<=h1) )
+		{
+			png_buf->row_pointers[(int)y][(i+w0)*3]   |= color&0xff;
+			png_buf->row_pointers[(int)y][(i+w0)*3+1] |= (color>>8)&0xff;
+			png_buf->row_pointers[(int)y][(i+w0)*3+2] |= (color>>16)&0xff;
+		}
 	}
 }
